9f5e9cb9fbc48f89c642b5e8d2b57d19d33d3ec3
Gabriel.Brito <gabrielbritodacruz@gmail.com>
2020-09-12 15:23:10 -0300

    Arquivos de teste da aula 1 até a 15

---
+++ python_teste/aula11.py
@@ -0,0 +1,5 @@
+nome = 'Gabriel'
+cores = {'limpa': '\033[m',
+         'azul': '\033[34m',
+         'amarelo': '\033[33m'}
+print('Olá! Muito prazer em te conhecer, {}{}{}'.format('\033[4;34m', nome, '\033[m'))
